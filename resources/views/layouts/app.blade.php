f79c6a9b5395d2a8f936f6713ab1e5b63f0a85e2
Aleksey <alekseykarpelev@gmail.com>
2022-07-06 18:08:19 +1000

    fix user  id2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -288,9 +288,11 @@
                     <!-- <li class="flex items-center">
                       <a class="inline-block px-8 py-2 mb-0 mr-4 font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro border-fuchsia-500 ease-soft-in text-size-xs hover:scale-102 active:shadow-soft-xs text-fuchsia-500 hover:border-fuchsia-500 active:bg-fuchsia-500 active:hover:text-fuchsia-500 hover:text-fuchsia-500 tracking-tight-soft hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
                     </li> -->
+                    @auth
                     <li class="flex items-center" >
                       <span class=" px-4" >{{ Auth::user()->name }}</span>
                     </li>
+                    @endauth
                     <li class="flex items-center">
                         <i class="fa fa-user sm:mr-1"></i>
                         <a class="block px-0 py-2 font-semibold transition-all ease-nav-brand text-size-sm text-slate-500" href="{{ route('logout') }}"
